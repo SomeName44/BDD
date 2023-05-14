807ee8d7440444eae69f31cbfc964aba187efac3
Olga <deputat.d@yandex.com>
2023-05-14 15:33:43 +0300

    change build gradle

--- src/test/java/ru/netology/web/test/MoneyTransferNegativeTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferNegativeTest.java
@@ -5,9 +5,6 @@ import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPage;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.web.data.DataHelper.*;
 
